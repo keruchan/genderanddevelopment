69e96b415cfc81316916694ac3eed79c3c06e080
keruchan <joshleones27@gmail.com>
2025-06-03 15:34:23 +0800

    46th fixing bug

--- admin_view_violations.php
+++ admin_view_violations.php
@@ -3,6 +3,7 @@ session_start();
 require 'connecting/connect.php';
 
 $search = isset($_GET['search']) ? trim($_GET['search']) : '%';
+echo "<pre>Search: " . htmlspecialchars($search) . "</pre>";
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['resolve_user_id'])) {
     $resolve_user_id = intval($_POST['resolve_user_id']);
