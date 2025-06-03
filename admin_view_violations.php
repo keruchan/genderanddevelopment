55480d2c0dfc827ff39541ae548d1b52383e3722
keruchan <joshleones27@gmail.com>
2025-06-03 15:33:16 +0800

    46th fixing violation fetch

--- admin_view_violations.php
+++ admin_view_violations.php
@@ -2,7 +2,7 @@
 session_start();
 require 'connecting/connect.php';
 
-$search = isset($_GET['search']) ? trim($_GET['search']) : '';
+$search = isset($_GET['search']) ? trim($_GET['search']) : '%';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['resolve_user_id'])) {
     $resolve_user_id = intval($_POST['resolve_user_id']);
